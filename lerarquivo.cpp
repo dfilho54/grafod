308bbc619ad93edafae3ed82682da323768e1ac8
dfilho54 <34212074+dfilho54@users.noreply.github.com>
2018-07-11 18:37:39 -0300

    Add files via upload

---
+++ lerarquivo.cpp
@@ -0,0 +1,42 @@
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+ int length;
+ char * buffer;
+
+ ifstream is;
+ is.open ("jogadores.csv", ios::in);
+
+ is.seekg (0, ios::end);
+ length = is.tellg();
+ is.seekg (0, ios::beg);
+
+ if (is.is_open())
+ {
+    int n = 0;
+    buffer = new char[length];
+    is.read(buffer,length);
+   
+   if (length > 0) 
+   {
+      while(n < length) //isso substitue o ";" para um espaço
+      {
+         n++;
+         if (buffer[n] == ';') buffer[n] = '\t';
+      }
+  
+      printf("RECODES: \nnome: \t\tpontos: \n%s", buffer);
+      getch();
+      delete []buffer;
+   }
+ }
+ else {
+   printf("Arquivo não foi aberto");
+   getch();
+}
+
+  is.close();
+}
